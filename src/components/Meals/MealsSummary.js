aa2d0078c50ecb1bfd64c65f7f0ab19c936025da
Monali C G <cgmonali@gmail.com>
2023-08-30 12:53:43 +0530

    Adding a Form(button)

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -3,10 +3,11 @@ import classes from './MealsSummary.module.css';
 const MealsSummary = () => {
   return (
     <section className={classes.summary}>
-      <h2>Delicious Food, Delivered To You</h2>
+      <h2>Your Health is our concern!!</h2>
       <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
+        Have highly delicious and healthy food 
+        Food is delivered within minutes
+        
       </p>
       <p>
         All our meals are cooked with high-quality ingredients, just-in-time and
